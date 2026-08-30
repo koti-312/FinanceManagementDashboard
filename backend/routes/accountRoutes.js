a363b457fa9df7b689174eb5943b62dccebdb4b6
koteswar singh <koteswar31204@gmail.com>
2026-08-30 21:26:15 +0530

    chore: remove backend folder from frontend branch

--- backend/routes/accountRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { authMiddleware } from "../middleware/authMiddleware.js"
-import { getAccounts } from "../controllers/accountController.js"
-
-const router=express.Router()
-
-router.get("/",authMiddleware,getAccounts)
-
-export default router
--- backend/routes/transactionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { authMiddleware } from "../middleware/authMiddleware.js"
-import { getTransactions } from "../controllers/transactionController.js"
-
-const router=express.Router()
-
-router.get("/",authMiddleware,getTransactions)
-
-export default router
